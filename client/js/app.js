e1371c4f4ec3cb90db848fe8df144d86a1da8744
ktjackbelle <KatieAPhillips@Katies-MacBook-Air.local>
2015-12-08 10:04:14 -0700

    finished assignment

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,72 @@
-angular.module('RepsApp', [])
-  .controller('RepsController', function(){
-    this.foo= "bar";
-    console.log('foo')
-  })
+angular.module('RepsApp', [
+  'RepsAppControllers'
+]);
+
+angular
+  .module('RepsAppControllers', [
+    'repsService'
+  ])
+  .controller('MainCtrl', function (reps) {
+    var main = this;
+    main.reps = [];
+    main.congressType = 'reps'; // or sens
+
+    main.loading = false;
+
+    main.apis = [
+      {
+        label: 'Zip',
+        method: function (zip) {
+          main.loading = true;
+          reps('all', 'zip', zip).then(function (data) {
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      },
+      {
+        label: 'Last Name',
+        method: function (name) {
+          main.loading = true;
+          reps(main.congressType, 'name', name).then(function (data) {
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      },
+      {
+        label: 'State',
+        method: function (state) {
+          main.loading = true;
+          reps(main.congressType, 'state', state).then(function (data) {
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      }
+    ];
+
+    main.criteria = main.apis[0];
+  });
+
+angular
+  .module('repsService', [])
+  .factory('reps', function ($http) {
+    var host = 'http://dgm-representatives.herokuapp.com';
+
+    /**
+     * @function search
+     * @param {String} type - can be "all", "reps", "sens"
+     * @param {String} criteria - can by "zip", "name", "state"
+     * @param {String} query - can any string
+     */
+    function search(type, criteria, query) {
+      return $http
+        .get(host + '/' + type + '/by-' + criteria + '/' + query)
+        .then(function (response) {
+          return response.data;
+        });
+    }
+
+    return search;
+  });
